dd22ac53488b5901fc10338aa3aff50022598d98
Abraham Galindo <abraham.galindo@gmail.com>
2023-08-31 08:57:43 -0600

    Build and export componets as lib

---
+++ custom-library/scripts/link.sh
@@ -0,0 +1,4 @@
+npm run build
+npm link
+echo '✅ Now you can link this package with "npm link custom-library"'
+echo ' '
--- custom-library/vite.config.ts
+++ custom-library/vite.config.ts
@@ -1,10 +1,34 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import * as localPackage from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      exclude: [
+        'src/App.tsx',
+        'src/main.tsx'
+      ]
+    })
+  ],
   server: {
     port: 8080,
-  }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'CustomLibrary',
+      formats: [ 'es' ],
+      fileName: (format) => `custom-library-${format}.js`
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(localPackage.peerDependencies)
+      ]
+    }
+  },
 })
